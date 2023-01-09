002f698905718180185fcc768d508edf37781c51
ben adelman <109058028+bena777@users.noreply.github.com>
2023-01-09 17:56:38 -0500

    Update base_game.py

--- base_game.py
+++ base_game.py
@@ -1,5 +1,6 @@
 import random
 balance=1000
+name= input("What is your name?")
 def blackjack():
     global balance
     # defines values in a dict while making a list out of 4 of each card (no suits)
@@ -15,33 +16,35 @@ def blackjack():
                 print("Sorry, not enough money with current balance of $"+str(balance),"please pick a new amount.")
                 self.bet=int(input("How much would you like to bet?"))
             self.hand=[]
-            card1=random.choice(deck)
-            deck.remove(card1)
-            card2=random.choice(deck)
-            deck.remove(card2)
-            self.hand.append(card1)
-            self.hand.append(card2)
+            card='A'
+            deck.remove(card)
+            card='A'
+            deck.remove(card)
+            self.hand.append(card)
+            self.hand.append(card)
             self.total=0
-            self.total+=card_values[card1]+card_values[card2]
-            print("Your cards are a",card1,"and a",card2,"for a total of",str(self.total))
+            self.total+=card_values[self.hand[0]]+card_values[self.hand[1]]
+            print("Your cards are a",self.hand[0],"and a",self.hand[1],"for a total of",str(self.total))
+            if self.total>21:
+                print("Pocket aces! Score will be adjusted as need be. Current score is 2.")
         # hit function adds another card to players hand
         def hit(self):
             card=random.choice(deck)
             deck.remove(card)
             self.hand.append(card)
-            self.total+=card_values[card]
+            self.total+=card_values[self.hand[-1]]
             print("You drew a",card,"bringing your total to",str(self.total))
-    # dealer class defines dealer's first card 
+    # dealer class defines dealer's first card
     class Dealer():
         def __init__(self):
             self.hand=[]
-            card1=random.choice(deck)
-            self.hand.append(card1)
-            deck.remove(card1)
+            card=random.choice(deck)
+            self.hand.append(card)
+            deck.remove(card)
             self.total=0
-            self.total+=card_values[card1]
-            print("Dealer draws a",card1,"for a total of",str(self.total))
-        # hit function is same as player hit function; would like to combine the two if possible 
+            self.total+=card_values[self.hand[-1]]
+            print("Dealer draws a",card,"for a total of",str(self.total))
+        # hit function is same as player hit function; would like to combine the two if possible
         def hit(self):
             card=random.choice(deck)
             self.hand.append(card)
@@ -49,18 +52,18 @@ def blackjack():
             self.total+=card_values[card]
             print("Dealer drew a",card,"bringing his total to",str(self.total))
     # makes variable out of player and dealer classes; 'Ben' is just a placeholder and can be changed
-    player=Player('Ben',Player.bet)
+    player=Player(name,Player.bet)
     dealer=Dealer()
     hit='Yes'
     busted=False
     # While the player keeps inputting 'Yes' they will continue to recieve another card to their total
-    # Loop will end when player decides he doesn't want to draw anymore or his score>21 
-    # If score>21, player loses his original bet 
+    # Loop will end when player decides he doesn't want to draw anymore or his score>21
+    # If score>21, player loses his original bet
     while hit.lower()=='yes':
         hit=input("Would you like to hit?")
         if hit=='Yes':
             player.hit()
-            # for loop changes players Ace card value from an 11 to a 1 if need be 
+            # for loop changes players Ace card value from an 11 to a 1 if need be
             if player.total>21:
                 for i in player.hand:
                     if i=='A':
@@ -74,7 +77,7 @@ def blackjack():
                     print("You lost $"+str(player.bet),"bringing your balance down to $"+str(balance))
                     # player.total=0
                     busted=True
-    # only activates if player does not bust while drawing 
+    # only activates if player does not bust while drawing
     # will continue drawing cards until dealer.total>=17
     while dealer.total<=17 and busted==False:
         if busted==False:
